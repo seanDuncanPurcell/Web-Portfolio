3642e6814e94c0b4c24b0e72eb6054fcbc009424
Sean D Purcell <sean.d.purcell@gmail.com>
2021-01-16 16:34:43 -0800

    React: main user menue implimented

---
+++ public/scripts/react-user-menu.js
@@ -0,0 +1,77 @@
+const Buttons = props => props.btns.map(btn => {
+  /*#__PURE__*/
+  React.createElement("a", {
+    href: btn.href
+  }, btn.name);
+});
+
+getUser = () => JSON.parse(sessionStorage.getItem('user'));
+
+class UserMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.user = JSON.parse(sessionStorage.getItem('user'));
+    this.state = {
+      clicked: false,
+      btns: []
+    };
+    this.user = {};
+    this.handleClick = this.handleClick.bind(this);
+    this.handleMenuOpen = this.handleMenuOpen.bind(this);
+  }
+
+  handleClick() {
+    this.user = getUser();
+
+    if (this.state.clicked) {
+      this.setState({
+        btns: [],
+        clicked: false
+      });
+    } else {
+      this.handleMenuOpen();
+      this.setState({
+        clicked: true
+      });
+    }
+  }
+
+  handleMenuOpen() {
+    if (this.user.loggedin) {
+      this.setState({
+        btns: [/*#__PURE__*/React.createElement("a", {
+          key: "mlob",
+          href: "/?out=true",
+          className: "btn-lvl-one"
+        }, "Log Out")]
+      });
+    } else {
+      this.setState({
+        btns: [/*#__PURE__*/React.createElement("a", {
+          key: "mlib",
+          href: "/login",
+          className: "btn-lvl-one"
+        }, "Log In"), /*#__PURE__*/React.createElement("a", {
+          key: "mnub",
+          href: "/register",
+          className: "btn-lvl-one"
+        }, "New Account")]
+      });
+    }
+  }
+
+  render() {
+    return /*#__PURE__*/React.createElement("div", {
+      id: "user-menu"
+    }, /*#__PURE__*/React.createElement("button", {
+      className: "btn-lvl-one",
+      onClick: this.handleClick
+    }, /*#__PURE__*/React.createElement("img", {
+      src: "img/icon-svg/white/user-icon.svg",
+      alt: "User Menu Icon"
+    })), this.state.btns);
+  }
+
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(UserMenu, null), document.getElementById('header-right'));
---
+++ src/react-user-menu.jsx
@@ -0,0 +1,52 @@
+const Buttons = props => props.btns.map( (btn) => {<a href={btn.href}>{btn.name}</a>} )
+
+getUser = () => JSON.parse(sessionStorage.getItem('user'))
+
+class UserMenu extends React.Component{
+    constructor(props){
+        super(props)
+        this.user = JSON.parse(sessionStorage.getItem('user'));
+        this.state = {clicked: false, btns: []};
+        this.user = {};
+        
+        this.handleClick = this.handleClick.bind(this);
+        this.handleMenuOpen = this.handleMenuOpen.bind(this);
+    }
+
+    handleClick(){
+        this.user = getUser();
+        if(this.state.clicked){
+            this.setState({btns: [], clicked: false});
+        }
+        else {
+            this.handleMenuOpen();
+            this.setState({clicked: true});
+        }
+    }
+
+    handleMenuOpen(){
+        if(this.user.loggedin){
+            this.setState({btns: [
+                <a key='mlob' href='/?out=true' className='btn-lvl-one'>Log Out</a>
+            ]})
+        }else{
+            this.setState({btns: [
+                <a key='mlib' href='/login' className='btn-lvl-one'>Log In</a>,
+                <a key='mnub' href='/register' className='btn-lvl-one'>New Account</a>
+            ]})            
+        }
+    }
+
+    render(){
+        return(
+            <div id='user-menu'>
+                <button  className='btn-lvl-one' onClick={this.handleClick}>
+                    <img src="img/icon-svg/white/user-icon.svg" alt="User Menu Icon" />
+                </button>
+                {this.state.btns}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render( <UserMenu />, document.getElementById('header-right'));
