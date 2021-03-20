9044dfcbcfa8ca68222c215071e750da1eb3006f
Duncan <sean.d.purcell@gmail.com>
2021-03-20 12:36:51 -0700

    mod comment section to improve UX

--- public/scripts/react-blog/components/CommentBox.js
+++ public/scripts/react-blog/components/CommentBox.js
@@ -4,61 +4,84 @@ function userSessionData() {
   return JSON.parse(sessionStorage.getItem('user'));
 }
 
+async function cmtSet(id, pointer, txtCont) {
+  const cmtObj = {
+    text: txtCont,
+    pointer: pointer
+  };
+  let url = '/api/blog-comments';
+  if (id) url += `?id=${id}`;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(cmtObj)
+  });
+}
+
+async function cmtsGet() {
+  const query = location.search;
+  const urlQuery = new URLSearchParams(query);
+  const id = urlQuery.get('id');
+  const url = `/api/blog-comments?id=${id}`;
+  const responce = await fetch(url);
+  const retObj = await responce.json();
+  return retObj;
+}
+
+function TxtBox(props) {
+  const user = userSessionData().username;
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h3", null, user.username), /*#__PURE__*/React.createElement("span", {
+    className: "comment_text-area"
+  }, /*#__PURE__*/React.createElement("textarea", {
+    placeholder: "Write a comment...",
+    value: props.value,
+    onChange: props.handleTxtChange
+  }), /*#__PURE__*/React.createElement("button", {
+    onClick: evt => {
+      props.handleSubmit();
+    }
+  }, "Send")));
+}
+
 function CmtOrReply(props) {
   const [txtCont, setTxtCont] = useState();
   const [relpy, setReply] = useState(false);
   const loggedIn = userSessionData().loggedin;
+  const pointer = props.pointer;
+  const resetWithNewData = props.resetWithNewData;
 
   function handleReply() {
     if (relpy) setReply(false);else setReply(true);
   }
 
-  const user = userSessionData();
-
-  function handleTxt(evt) {
-    const val = evt.target.value;
-    setTxtCont(val);
-  }
-
-  function handleSubmit(props, id) {
-    const cmtObj = {
-      text: txtCont,
-      pointer: props.pointer
-    };
-    let url = '/api/blog-comments';
-    if (id) url += `?id=${id}`;
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(cmtObj)
-    });
+  async function handleSubmit(id) {
+    await cmtSet(id, pointer, txtCont);
     setTxtCont('');
     setReply(false);
-    props.onSubmit();
+    resetWithNewData();
+  }
+
+  function handleTxtChange(evnt) {
+    const val = evnt.target.value;
+    setTxtCont(val);
   }
 
   if (!loggedIn) return /*#__PURE__*/React.createElement(React.Fragment, null);else if (relpy) {
     return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("span", {
       className: "Comment-box__Sub-controls"
-    }, /*#__PURE__*/React.createElement("button", {
+    }, /*#__PURE__*/React.createElement("button", null, "Edit"), /*#__PURE__*/React.createElement("p", null, "\u2022"), /*#__PURE__*/React.createElement("button", {
       onClick: handleReply
-    }, "Reply")), /*#__PURE__*/React.createElement("h3", null, user.username), /*#__PURE__*/React.createElement("span", {
-      className: "comment_text-area"
-    }, /*#__PURE__*/React.createElement("textarea", {
-      placeholder: "Write a comment...",
+    }, "Reply")), /*#__PURE__*/React.createElement(TxtBox, {
+      handleTxtChange: handleTxtChange,
       value: txtCont,
-      onChange: handleTxt
-    }), /*#__PURE__*/React.createElement("button", {
-      onClick: evt => {
-        handleSubmit(props);
-      }
-    }, "Send")));
+      handleSubmit: handleSubmit
+    }));
   } else {
     return /*#__PURE__*/React.createElement("span", {
       className: "Comment-box__Sub-controls"
-    }, /*#__PURE__*/React.createElement("button", {
+    }, /*#__PURE__*/React.createElement("button", null, "Edit"), /*#__PURE__*/React.createElement("p", null, "\u2022"), /*#__PURE__*/React.createElement("button", {
       onClick: handleReply
     }, "Reply"));
   }
@@ -67,7 +90,7 @@ function CmtOrReply(props) {
 function CommentMainComp(props) {
   const level = props.level;
   const comments = props.array;
-  const onSubmit = props.onSubmit;
+  const resetWithNewData = props.resetWithNewData;
 
   if (comments.length > 0) {
     return /*#__PURE__*/React.createElement("div", null, comments.map((item, i) => {
@@ -86,11 +109,12 @@ function CommentMainComp(props) {
         className: "Comment-box__Sub-content"
       }, /*#__PURE__*/React.createElement("h3", null, item.authorID), /*#__PURE__*/React.createElement("p", null, item.text)), /*#__PURE__*/React.createElement(CmtOrReply, {
         pointer: item._id,
-        onSubmit: onSubmit
+        resetWithNewData: resetWithNewData
       }), /*#__PURE__*/React.createElement(CommentMainComp, {
         level: level + 1,
         array: item.comments,
-        srcKey: myKey
+        srcKey: myKey,
+        resetWithNewData: resetWithNewData
       })));
     }));
   } else {
@@ -105,10 +129,14 @@ class CommentBox extends React.Component {
     this.state = {
       reply: false,
       comments: [],
-      loggedIn: false
+      loggedIn: false,
+      newCmtTxt: ''
     };
-    this.handleComment = this.handleComment.bind(this);
+    this.handleShowCmtBox = this.handleShowCmtBox.bind(this);
+    this.handleTxtChange = this.handleTxtChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.resetWithNewData = this.resetWithNewData.bind(this);
+    this.CommentComposer = this.CommentComposer.bind(this);
   }
 
   async componentDidMount() {
@@ -120,9 +148,7 @@ class CommentBox extends React.Component {
 
       this.pointer = id; //us id to set up comment fetch
 
-      const url = `/api/blog-comments?id=${id}`;
-      const responce = await fetch(url);
-      const commentsObj = await responce.json(); //check user status
+      const commentsObj = await cmtsGet(); //check user status
 
       const loggedIn = userSessionData().loggedin; //set up state to reflect new infor
 
@@ -135,7 +161,7 @@ class CommentBox extends React.Component {
     }
   }
 
-  handleComment() {
+  handleShowCmtBox() {
     if (this.state.reply) this.setState({
       reply: false
     });else this.setState({
@@ -143,17 +169,48 @@ class CommentBox extends React.Component {
     });
   }
 
-  async handleSubmit() {
-    const query = location.search;
-    const urlQuery = new URLSearchParams(query);
-    const id = urlQuery.get('id');
-    const url = `/api/blog-comments?id=${id}`;
-    const responce = await fetch(url);
-    const commentsObj = await responce.json();
+  handleTxtChange(evnt) {
+    const val = evnt.target.value;
+    this.setState({
+      newCmtTxt: val
+    });
+  }
+
+  async handleSubmit(id) {
+    await cmtSet(id, this.pointer, this.state.newCmtTxt);
+    this.setState({
+      newCmtTxt: '',
+      reply: false
+    });
+    this.resetWithNewData();
+  }
+
+  async resetWithNewData() {
+    const commentsObj = await cmtsGet();
     this.setState({
       comments: commentsObj,
       reply: false
     });
+  }
+
+  CommentComposer(props) {
+    if (this.state.reply) {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+        className: "Comment-box__Controls"
+      }, /*#__PURE__*/React.createElement("button", {
+        onClick: this.handleShowCmtBox
+      }, "Leave Comment")), /*#__PURE__*/React.createElement(TxtBox, {
+        handleTxtChange: this.handleTxtChange,
+        value: this.state.newCmtTxt,
+        handleSubmit: this.handleSubmit
+      }));
+    } else {
+      return /*#__PURE__*/React.createElement("div", {
+        className: "Comment-box__Controls"
+      }, /*#__PURE__*/React.createElement("button", {
+        onClick: this.handleShowCmtBox
+      }, "Leave Comment"));
+    }
   }
 
   render() {
@@ -161,18 +218,18 @@ class CommentBox extends React.Component {
     const loggedIn = this.state.loggedIn;
 
     if (loggedIn) {
-      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(CmtOrReply, {
-        show: this.state.reply,
-        pointer: this.pointer,
-        onSubmit: this.handleSubmit
-      }), /*#__PURE__*/React.createElement(CommentMainComp, {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(this.CommentComposer, null), /*#__PURE__*/React.createElement(CommentMainComp, {
         level: 1,
         array: array,
         srcKey: 'cmnt-blck',
-        onSubmit: this.handleSubmit
+        resetWithNewData: this.resetWithNewData
       }));
     } else {
-      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(CommentMainComp, {
+      return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h4", {
+        style: {
+          color: "gray"
+        }
+      }, /*#__PURE__*/React.createElement("em", null, "Create an account or login to leave a comment.")), /*#__PURE__*/React.createElement(CommentMainComp, {
         level: 1,
         array: array,
         srcKey: 'cmnt-blck',
--- src/react-blog/components/CommentBox.jsx
+++ src/react-blog/components/CommentBox.jsx
@@ -3,71 +3,99 @@ const useState = React.useState
 function userSessionData () {
   return JSON.parse(sessionStorage.getItem('user'));
 }
+  
+async function cmtSet(id, pointer, txtCont){
+  const cmtObj = {
+    text: txtCont,
+    pointer: pointer
+  }
+  let url = '/api/blog-comments'
+  if (id) url += `?id=${id}`;
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(cmtObj)
+  })
+}
+
+async function cmtsGet(){
+  const query = location.search
+  const urlQuery = new URLSearchParams(query)
+  const id = urlQuery.get('id')
+  const url = `/api/blog-comments?id=${id}`
+  const responce = await fetch(url)
+  const retObj = await responce.json()
+  return retObj
+}
+
+function TxtBox (props){
+  const user = userSessionData().username
+  return(
+    <>  
+      <h3>{user.username}</h3>
+      <span className='comment_text-area'>
+        <textarea
+          placeholder='Write a comment...'
+          value={props.value}
+          onChange={props.handleTxtChange}
+        />
+        <button onClick={ evt => {
+          props.handleSubmit()
+        }}>
+          Send
+        </button>
+      </span>
+    </>
+  )
+}
 
 function CmtOrReply(props) {
   const [txtCont, setTxtCont] = useState()
   const [relpy, setReply] = useState(false)
   const loggedIn = userSessionData().loggedin
+  const pointer = props.pointer
+  const resetWithNewData = props.resetWithNewData
+
   function handleReply() {
     if(relpy) setReply(false)
     else setReply(true)
   }
-  const user = userSessionData()
-
-  function handleTxt(evt) {
-    const val = evt.target.value 
-    setTxtCont(val);
-  }
   
-  function handleSubmit(props, id){
-    const cmtObj = {
-      text: txtCont,
-      pointer: props.pointer
-    }
-    let url = '/api/blog-comments'
-    if (id) url += `?id=${id}`;
-    fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(cmtObj)
-    })
-
+  async function handleSubmit(id){
+    await cmtSet(id, pointer, txtCont)
     setTxtCont('')
     setReply(false)
-    props.onSubmit()
+    resetWithNewData()
+  }
+
+  function handleTxtChange(evnt) {
+    const val = evnt.target.value 
+    setTxtCont(val)
   }
 
   if(!loggedIn) return <></>
   else if(relpy){
     return(
       <>
-      <span className='Comment-box__Sub-controls'>
-        {/* <button>Like</button>
-        <p>&#8226;</p> */}
-        <button onClick={handleReply}>Reply</button>
-      </span>
-        <h3>{user.username}</h3>
-        <span className='comment_text-area'>
-          <textarea
-            placeholder='Write a comment...'
-            value={txtCont}
-            onChange={handleTxt}
-          />
-          <button onClick={ evt => {
-            handleSubmit(props)
-          }}>
-            Send
-          </button>
+        <span className='Comment-box__Sub-controls'>
+          <button>Edit</button>
+          <p>&#8226;</p>
+          <button onClick={handleReply}>Reply</button>
         </span>
+        <TxtBox 
+          handleTxtChange={handleTxtChange}
+          value={txtCont}
+          handleSubmit={handleSubmit}
+        />
       </>
     )
   }else{
     return (
       <span className='Comment-box__Sub-controls'>
-        {/* <button>Like</button>
-        <p>&#8226;</p> */}
+        <button>Edit</button>
+        <p>&#8226;</p>
         <button onClick={handleReply}>Reply</button>
       </span>
     )
@@ -77,7 +105,7 @@ function CmtOrReply(props) {
 function CommentMainComp (props){
   const level = props.level
   const comments = props.array
-  const onSubmit = props.onSubmit
+  const resetWithNewData = props.resetWithNewData
   if(comments.length > 0){
     return(
       <div>
@@ -95,12 +123,13 @@ function CommentMainComp (props){
                 </div>
                 <CmtOrReply 
                   pointer={item._id}
-                  onSubmit={onSubmit}
+                  resetWithNewData={resetWithNewData}
                 />
                 <CommentMainComp
                   level={level + 1}
                   array={item.comments}
                   srcKey={myKey}
+                  resetWithNewData={resetWithNewData}
                 />
               </div>
             </div>
@@ -117,9 +146,12 @@ class CommentBox extends React.Component{
   constructor(props){
     super(props)
     this.pointer = ''
-    this.state = {reply: false, comments: [], loggedIn: false}
-    this.handleComment = this.handleComment.bind(this)
+    this.state = {reply: false, comments: [], loggedIn: false, newCmtTxt: ''}
+    this.handleShowCmtBox = this.handleShowCmtBox.bind(this)
+    this.handleTxtChange = this.handleTxtChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.resetWithNewData = this.resetWithNewData.bind(this)
+    this.CommentComposer = this.CommentComposer.bind(this)
   } 
   
   async componentDidMount () {
@@ -133,9 +165,7 @@ class CommentBox extends React.Component{
       this.pointer = id
 
       //us id to set up comment fetch
-      const url = `/api/blog-comments?id=${id}`
-      const responce = await fetch(url)
-      const commentsObj = await responce.json()
+      const commentsObj = await cmtsGet()
 
       //check user status
       const loggedIn = userSessionData().loggedin
@@ -147,19 +177,48 @@ class CommentBox extends React.Component{
     }
   }
 
-  handleComment() {
+  handleShowCmtBox() {
     if(this.state.reply) this.setState({reply: false})
     else this.setState({reply: true})
   }
 
-  async handleSubmit(){
-    const query = location.search
-    const urlQuery = new URLSearchParams(query)
-    const id = urlQuery.get('id')
-    const url = `/api/blog-comments?id=${id}`
-    const responce = await fetch(url)
-    const commentsObj = await responce.json()
+  handleTxtChange(evnt){
+    const val = evnt.target.value 
+    this.setState({newCmtTxt: val})
+  }
+  
+  async handleSubmit(id){
+    await cmtSet(id, this.pointer, this.state.newCmtTxt)
+    this.setState({newCmtTxt: '', reply: false})
+    this.resetWithNewData()
+  }
+
+  async resetWithNewData(){
+    const commentsObj = await cmtsGet()
     this.setState({comments: commentsObj, reply: false })
+  }
+
+  CommentComposer(props){
+    if(this.state.reply){
+      return(
+        <>
+          <div className='Comment-box__Controls'>            
+            <button onClick={this.handleShowCmtBox}>Leave Comment</button>
+          </div>
+          <TxtBox 
+            handleTxtChange={this.handleTxtChange}
+            value={this.state.newCmtTxt}
+            handleSubmit={this.handleSubmit}
+          />
+        </>
+      )
+    }else{
+      return(
+        <div className='Comment-box__Controls'>            
+          <button onClick={this.handleShowCmtBox}>Leave Comment</button>
+        </div>
+      )
+    }
   }
 
   render(){
@@ -169,22 +228,19 @@ class CommentBox extends React.Component{
     if(loggedIn){
       return(
         <>
-          <CmtOrReply
-            show={this.state.reply}
-            pointer={this.pointer}
-            onSubmit={this.handleSubmit}
-          />
+          <this.CommentComposer />
           <CommentMainComp 
             level={1}
             array={array}
             srcKey={'cmnt-blck'}
-            onSubmit={this.handleSubmit}
+            resetWithNewData={this.resetWithNewData}
           />
         </>
       )
     } else {
       return(
         <>
+         <h4 style={{color: "gray"}}><em>Create an account or login to leave a comment.</em></h4>
           <CommentMainComp
             level={1}
             array={array}
